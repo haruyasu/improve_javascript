41b708898e02e77a88762b0144ac805ccd0f63c9
haruyasu <harukun2002@gmail.com>
2018-09-08 21:51:14 -0400

    'app'

--- 4-DOM-pig-game/app.js
+++ 4-DOM-pig-game/app.js
@@ -14,4 +14,12 @@ scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = 6;
+dice = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector('#current-' + activePlayer).textContent = dice;
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+var x = document.querySelector('#score-0').textContent;
+console.log(x);
+
+document.querySelector('.dice').style.display = 'none';
