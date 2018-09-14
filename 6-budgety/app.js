121cc02cc446a709ba5cd65bf2b5338282fd12ef
haruyasu <harukun2002@gmail.com>
2018-09-13 22:38:47 -0400

    Adding a New item to Our Budget Controller

--- 6-budgety/app.js
+++ 6-budgety/app.js
@@ -21,7 +21,30 @@ var budgetController = (function() {
             exp: 0,
             inc: 0
         }
-    }
+    };
+
+    return {
+        addItem: function(type, des, val) {
+            var newItem, ID;
+
+            // Create new ID
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            // Create nwe item based on 'inc' or 'exp' type
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Expense(ID, des, val);
+            }
+
+            data.allItems[type].push(newItem);
+            return newItem;
+        }
+    };
 
 })();
 
@@ -63,10 +86,13 @@ var controller = (function(budgetCtrl, UICtrl) {
     };
 
     var ctrlAddItem = function() {
+        var input, newItem;
+
         // 1. Get the field input data
-        var input = UICtrl.getInput();
+        input = UICtrl.getInput();
 
         // 2. Add the item to the budget CONTROLLER
+        newItem = budgetController.addItem(input.type, input.description, input.value);
 
         // 3. Add the item to the UI
 
