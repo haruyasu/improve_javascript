787638b38529e1743622cc789b7bd40404bc3bf6
haruyasu <harukun2002@gmail.com>
2018-09-20 21:35:09 -0400

    Updating the Pecentages UI Controller

--- 6-budgety/app.js
+++ 6-budgety/app.js
@@ -4,6 +4,19 @@ var budgetController = (function() {
         this.id = id;
         this.description = description;
         this.value = value;
+        this.percentage = -1;
+    };
+
+    Expense.prototype.calcPercentage = function(totalIncome) {
+        if (totalIncome > 0) {
+            this.percentage = Math.round((this.value / totalIncome) * 100);
+        } else {
+            this.percentage = -1;
+        }
+    };
+
+    Expense.prototype.getPercentage = function() {
+        return this.percentage;
     };
 
     var Income = function(id, description, value) {
@@ -87,8 +100,19 @@ var budgetController = (function() {
             }
 
             // Expense = 100 and income 200, spent 50% = 100/200 = 0.5 * 100
+        },
 
+        calculatePercentages: function() {
+            data.allItems.exp.forEach(function(cur) {
+                cur.calcPercentage(data.totals.inc);
+            });
+        },
 
+        getPercentages: function() {
+            var allPerc = data.allItems.exp.map(function(cur) {
+                return cur.getPercentage();
+            });
+            return allPerc;
         },
 
         getBudget: function() {
@@ -220,11 +244,13 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     var updatePercentages = function() {
         // 1. Calculate percentages
-
+        budgetCtrl.calculatePercentages();
+        
         // 2. Read percentages from the budget controller
+        var percentages = budgetCtrl.getPercentages();
 
         // 3. Update the UI with the new percentages
-
+        console.log(percentages);
     };
 
     var ctrlAddItem = function() {
