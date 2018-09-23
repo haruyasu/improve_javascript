3c8748fd60133c848c3e47a64d4edfcb4874c8cb
haruyasu <harukun2002@gmail.com>
2018-09-23 14:24:21 -0400

    Finishing Touches Improving the UX

--- 6-budgety/app.js
+++ 6-budgety/app.js
@@ -165,6 +165,12 @@ var UIController = (function() {
 
     };
 
+    var nodeListForEach = function(list, callback) {
+        for (var i = 0; i < list.length; i++) {
+            callback(list[i], i);
+        }
+    };
+
     return {
         getInput: function() {
             return {
@@ -231,12 +237,6 @@ var UIController = (function() {
 
         displayPercentages: function(percentages) {
             var fields = document.querySelectorAll(DOMStrings.expensesPercLabel);
-            
-            var nodeListForEach = function(list, callback) {
-                for (var i = 0; i < list.length; i++) {
-                    callback(list[i], i);
-                }
-            };
 
             nodeListForEach(fields, function(current, index) {
                 if (percentages[index] > 0) {
@@ -254,6 +254,19 @@ var UIController = (function() {
             month = now.getMonth();
             year = now.getFullYear();
             document.querySelector(DOMstrings.dataLabel).textContent = monthes[month] + ' ' + year;
+        },
+
+        changedType: function() {
+            var fields = document.querySelectorAll(
+                DOMstrings.inputType + ',' +
+                DOMstrings.inputDescription + ',' +
+                DOMstrings.inputValue);
+            
+            nodeListForEach(fields, function(cur) {
+                cur.classList.toggle('red-focus');
+            });
+
+            document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
         },
 
         getDOMstrings: function() {
@@ -275,8 +288,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         });
 
         document.querySelector(DOM.container).addEventListener('click', ctrDeleteItem);
-
-
+        document.querySelector(DOM.container).addEventListener('change', UICtrl.changedType);
     };
 
     var updateBudget = function() {
