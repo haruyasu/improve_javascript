85b5c3270fe57ee9623d57a902fbe079ecd1dc59
haruyasu <harukun2002@gmail.com>
2018-10-03 22:08:34 -0400

    Building the Search Controller

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -1,16 +1,5 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query) {
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-    const key = '457cba8e1e6a73f37d495070b687ab06';
-    try {
-        const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-        const recipes = res.data.recipes;
-        console.log(recipes);    
-    } catch (error) {
-        alert(error);
-    }
-}
-getResults('pizza');
-
-
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
