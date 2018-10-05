385f294a4a1ebad84de89197c019296844269db5
haruyasu <harukun2002@gmail.com>
2018-10-04 22:01:27 -0400

    Building the Search View

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -1,24 +1,29 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 
 const state = {};
 
 const controlSearch = async () => {
-    const query = 'pizza'; //todo
+    const query = searchView.getInput();
+
     if (query) {
         // New search object and add to state
         state.search = new Search(query);
 
         // Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
 
         // Search for recipies
         await state.search.getResults();
 
         // render results on UI
-        console.log(state.search.result);
+        searchView.renderResults(state.search.result);
     }
 }
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
 });
