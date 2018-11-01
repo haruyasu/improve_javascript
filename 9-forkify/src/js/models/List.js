4197a4297fb13b51c7d42a7cedf951aee917453c
haruyasu <harukun2002@gmail.com>
2018-10-31 20:43:42 -0400

    storage list

--- 9-forkify/src/js/models/List.js
+++ 9-forkify/src/js/models/List.js
@@ -13,15 +13,30 @@ export default class List {
             ingredient
         }
         this.items.push(item);
+        this.persistData();
         return item;
     }
 
     deleteItem(id) {
         const index = this.items.findIndex(el => el.id === id);
         this.items.splice(index, 1);
+        // this.persistData();
     }
 
     updateCount(id, newCount) {
         this.items.find(el => el.id === id).count = newCount;
+        // this.persistData();
+    }
+
+    persistData() {
+        localStorage.setItem('items', JSON.stringify(this.items));
+        console.log(this.items)
+    }
+
+    readStorage() {
+        const storage = JSON.parse(localStorage.getItem('items'));
+
+        // Restoring list from the localStorage
+        if (storage) this.items = storage;
     }
 }
