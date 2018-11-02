91ab7715d2d6d418a674911d89bea0381db0b6c9
haruyasu <harukun2002@gmail.com>
2018-11-01 20:56:21 -0400

    listitem

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -95,6 +95,7 @@ const controlRecipe = async () => {
 const controlList = () => {
     // Create a new list IF there in none yet
     if (!state.list) state.list = new List();
+    state.list.readStorage();
 
     // Add each ingredient to the list and UI
     state.recipe.ingredients.forEach(el => {
@@ -106,6 +107,7 @@ const controlList = () => {
 // Handle delete and update list item events
 elements.shopping.addEventListener('click', e => {
     const id = e.target.closest('.shopping__item').dataset.itemid;
+    state.list.readStorage();
 
     // Handle the delete button
     if (e.target.matches('.shopping__delete, .shopping__delete *')) {
--- 9-forkify/src/js/models/List.js
+++ 9-forkify/src/js/models/List.js
@@ -20,12 +20,12 @@ export default class List {
     deleteItem(id) {
         const index = this.items.findIndex(el => el.id === id);
         this.items.splice(index, 1);
-        // this.persistData();
+        this.persistData();
     }
 
     updateCount(id, newCount) {
         this.items.find(el => el.id === id).count = newCount;
-        // this.persistData();
+        this.persistData();
     }
 
     persistData() {
