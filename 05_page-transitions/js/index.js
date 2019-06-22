cd024528a5f1e9593977465b7ca2076f88b1e7ca
haruyasu <harukun2002@gmail.com>
2019-06-21 22:51:53 -0400

    Finish smooth transitions

--- 05_page-transitions/js/index.js
+++ 05_page-transitions/js/index.js
@@ -1,3 +1,9 @@
-const run = () => {
-    console.log("test");
-};
+import Highway from '@dogstudio/highway';
+import Fade from './transition';
+
+const H = new Highway.Core({
+    transitions: {
+        default: Fade
+    }
+});
+
