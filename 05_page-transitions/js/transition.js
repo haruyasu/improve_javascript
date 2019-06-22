4ab2df632d20547d414235aba2943d2a164af6b2
haruyasu <harukun2002@gmail.com>
2019-06-22 13:42:45 -0400

    Highway

--- 05_page-transitions/js/transition.js
+++ 05_page-transitions/js/transition.js
@@ -4,10 +4,22 @@ import {TimelineLite} from 'gsap';
 class Fade extends Highway.Transition {
     in ({from, to, done}) {
         const tl = new TimelineLite();
-        tl.fromTo(to, 0.2, { left: "-100%", top: "50%" }, { left: "0%" }).fromTo(
+        tl.fromTo(
+            to, 
+            0.2, 
+            { 
+                left: "-100%", 
+                top: "50%" 
+            }, 
+            { 
+                left: "0%" 
+            }
+        ).fromTo(
             to,
             0.2,
-            { height: "2vh" },
+            { 
+                height: "2vh" 
+            },
             {
                 height: "90vh",
                 top: "10%",
@@ -16,8 +28,16 @@ class Fade extends Highway.Transition {
                     done();
                 }
             }
-        )
-        .fromTo(to.children[0], 2, {opacity: 0}, {opacity: 1});
+        ).fromTo(
+            to.children[0], 
+            2, 
+            {
+                opacity: 0
+            },
+            {
+                opacity: 1
+            }
+        );
     }
 
     out ({from, done}) {
